d3ee80ee4afa544149e510610bd34b944b9de2c1
Cristian <2912196670108@ingenieria.usac.edu.gt>
2021-10-06 16:22:21 -0600

    [ADD] Inicio proyecto

---
+++ Consultas.sql
@@ -0,0 +1,77 @@
+
+-- =================================================================================
+-- 1. Desplegar para cada elección el país y el partido político que obtuvo mayor
+-- porcentaje de votos en su país. Debe desplegar el nombre de la elección, el
+-- año de la elección, el país, el nombre del partido político y el porcentaje que
+-- obtuvo de votos en su país.
+-- =================================================================================
+
+-- =================================================================================
+-- 2. Desplegar total de votos y porcentaje de votos de mujeres por departamento
+-- y país. El ciento por ciento es el total de votos de mujeres por país. (Tip:
+-- Todos los porcentajes por departamento de un país deben sumar el 100%)
+-- =================================================================================
+
+-- =================================================================================
+-- 3. Desplegar el nombre del país, nombre del partido político y número de
+-- alcaldías de los partidos políticos que ganaron más alcaldías por país.
+-- =================================================================================
+
+-- =================================================================================
+-- 4. Desplegar todas las regiones por país en las que predomina la raza indígena.
+-- Es decir, hay más votos que las otras razas.
+-- =================================================================================
+
+-- =================================================================================
+-- 5. Desplegar el nombre del país, el departamento, el municipio y la cantidad de
+-- votos universitarios de todos aquellos municipios en donde la cantidad de
+-- votos de universitarios sea mayor que el 25% de votos de primaria y menor
+-- que el 30% de votos de nivel medio. Ordene sus resultados de mayor a
+-- menor.
+-- =================================================================================
+
+-- =================================================================================
+-- 6. Desplegar el porcentaje de mujeres universitarias y hombres universitarios
+-- que votaron por departamento, donde las mujeres universitarias que votaron
+-- fueron más que los hombres universitarios que votaron.
+-- =================================================================================
+
+-- =================================================================================
+-- 7. Desplegar el nombre del país, la región y el promedio de votos por
+-- departamento. Por ejemplo: si la región tiene tres departamentos, se debe
+-- sumar todos los votos de la región y dividirlo dentro de tres (número de
+-- departamentos de la región).
+-- =================================================================================
+
+-- =================================================================================
+-- 8. Desplegar el total de votos de cada nivel de escolaridad (primario, medio,
+-- universitario) por país, sin importar raza o sexo.
+-- =================================================================================
+
+-- =================================================================================
+-- 9. Desplegar el nombre del país y el porcentaje de votos por raza.
+-- =================================================================================
+
+-- =================================================================================
+-- 10.Desplegar el nombre del país en el cual las elecciones han sido más
+-- peleadas. Para determinar esto se debe calcular la diferencia de porcentajes
+-- de votos entre el partido que obtuvo más votos y el partido que obtuvo menos
+-- votos.
+-- =================================================================================
+
+-- =================================================================================
+-- 11. Desplegar el total de votos y el porcentaje de votos emitidos por mujeres
+-- indígenas alfabetas.
+-- =================================================================================
+
+-- =================================================================================
+-- 12.Desplegar el nombre del país, el porcentaje de votos de ese país en el que
+-- han votado mayor porcentaje de analfabetas. (tip: solo desplegar un nombre
+-- de país, el de mayor porcentaje).
+-- =================================================================================
+
+-- =================================================================================
+-- 13.Desplegar la lista de departamentos de Guatemala y número de votos
+-- obtenidos, para los departamentos que obtuvieron más votos que el
+-- departamento de Guatemala.
+-- =================================================================================
---
+++ ScriptCreacion.sql
@@ -0,0 +1,19 @@
+CREATE TABLE TEMPORARY (
+    nombre_eleccion     VARCHAR(200),
+    ano_eleccion        VARCHAR(100),
+    pais                VARCHAR(100),
+    region              VARCHAR(100),
+    depto               VARCHAR(100),
+    municipio           VARCHAR(100),
+    partido             VARCHAR(100),
+    nombre_partido      VARCHAR(100),
+    sexo                VARCHAR(100),
+    raza                VARCHAR(100),
+    analfabetos         VARCHAR(100),
+    alfabetos           VARCHAR(100),
+    sexo                VARCHAR(100),
+    raza                VARCHAR(100),
+    primaria            VARCHAR(100),
+    nivel_medio         VARCHAR(100),
+    universitarios      VARCHAR(100)
+);
